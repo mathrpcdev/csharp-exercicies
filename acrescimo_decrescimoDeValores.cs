166e7116d16115a4548a29524ff2ed2f301e857b
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-10-31 16:18:32 -0300

    Melhoria de código e readability 31/10/22

--- acrescimo_decrescimoDeValores.cs
+++ acrescimo_decrescimoDeValores.cs
@@ -6,17 +6,9 @@ namespace Matheus
     {
         static void Main(string[] args)
         {
+            // ----------------------------------------*EXERCÍCIO DE ACRESCIMO E DECRESCIMO DE VALORES*-----------------------------------------------
 
-            Console.WriteLine(5 / 10);
-        }
-    }
-}
-
-/*          
-
-        ----------------------------------------*EXERCÍCIO DE ACRESCIMO E DECRESCIMO DE VALORES*-----------------------------------------------
-
-  int value = 5;
+            int value = 5;
 
             value = value + 5;
             Console.WriteLine("Primeira incrementação: " + value);
@@ -29,6 +21,7 @@ namespace Matheus
 
             Console.WriteLine("---------------------------------------------------------");
 
+            //5+16-5=11;
             value = value - 5;
             Console.WriteLine("Primeiro decrescimento: " + value);
 
@@ -37,59 +30,43 @@ namespace Matheus
 
             value--;
             Console.WriteLine("Terceiro decrescimo: " + value);
+            Console.WriteLine();
 
+            Console.WriteLine(
+                "------------*ACRESCENTANDO APENAS UM VALOR*-----------------------------"
+            );
 
-Console.WriteLine("------------*ACRESCENTANDO APENAS UM VALOR*-----------------------------");
+            int valor = 1;
+            valor++;
+            Console.WriteLine("Primeiro: " + valor);
+            Console.WriteLine("Segundo: " + valor++);
+            Console.WriteLine("Terceiro: " + valor);
+            Console.WriteLine("Quarto: " + (++valor));
 
-            int value = 1;
-            value++;
-            Console.WriteLine("Primeiro: " + value);
-            Console.WriteLine("Segundo: " + value++);
-            Console.WriteLine("Terceiro: " + value);
-            Console.WriteLine("Quarto: " + (++value));
+            Console.WriteLine(
+                "------------------DIMINUINDO(DECRESCENDO)UM VALOR QUALQUER(Ex: Diminuindo --um)"
+            );
 
-Console.WriteLine("------------------DIMINUINDO(DECRESCENDO)UM VALOR QUALQUER\{(Ex: Diminuindo --um ")\n;
-            int value = 1;
-            value = 1;
-            value--;
-            Console.WriteLine("Primeiro: " + value);
-            Console.WriteLine("Segundo: " + value++);
-            Console.WriteLine("Terceiro: " + value);
-            Console.WriteLine("Quarto: " + (++value));
+            int value1 = 1;
+            value1 = 1;
+            value1--;
 
+            Console.WriteLine("Primeiro: " + value1);
+            Console.WriteLine("Segundo: " + value1++);
+            Console.WriteLine("Terceiro: " + value1);
+            Console.WriteLine("Quarto: " + (++value1));
 
-            int value = 1;
-            value = 1;
-            value--;
-            Console.WriteLine("Primeiro: " + value);
-            Console.WriteLine("Segundo: " + value--);
-            Console.WriteLine("Terceiro: " + value);
-            Console.WriteLine("Quarto: " + (--value));
-///
-/// / //       }
+            int value2 = 2;
+            value2 = 2;
+            value2--;
+
+            Console.WriteLine("Primeiro: " + value2);
+            Console.WriteLine("Segundo: " + value2--);
+            Console.WriteLine("Terceiro: " + value2);
+            Console.WriteLine("Quarto: " + (--value2));
+        }
     }
-} */
-
-//* -----------------CONVERTENDO FAHRENHEIT PARA CELCIUS------------------------------------------------------------------------
-/*int fahrenheit = 94;
-fahrenheit = fahrenheit - 32;
-int celcius = 0;
-celcius = fahrenheit;
-
-
-
-Console.WriteLine("94 - 32 = " + celcius);
-Console.WriteLine("The temperature is: 62 * (5/9) = " + celcius * 5 / 9m + " Celcius");
-
------------MS Solution------------
-
-int fahrenheit = 94;
-decimal celcius = (fahrenheit - 32m) * (5m / 9m);
-
-Console.WriteLine("The temperature is " + celcius + " Celcius");
-
-----------------------------------------------------------------------------------
-*/
+}
 
 
 
