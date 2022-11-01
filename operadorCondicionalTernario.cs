e9ebfbc6950fd5b1958974b02528f3a901c6ccfe
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-01 12:47:06 -0300

    Melhorias no código e readability.

--- operadorCondicionalTernario.cs
+++ operadorCondicionalTernario.cs
@@ -4,14 +4,12 @@ class testandoCondicionais
 {
     static void Main(string[] args)
     {
-        /* Neste exercício utilizo estruturas de decisão e interpolação para uso das variáveis no Console.WriteLine.
+        /* Neste challenge utilizo estruturas de decisão e interpolação para uso das variáveis no Console.WriteLine.
         Depois faço o mesmo exercício que dá o mesmo resultado, utilizando operadores condicionais  < X > Y ? SIM : ou NAO >
         Conhecidos também como Operador "Condicional Ternário".
         ** USE O OPERADOR CONDICIONAL QUANDO PRECISAR ADICIONAR A LÓGICA DE RAMIFICAÇÃO INLINE. ** */
 
-        Console.WriteLine(
-            $"***** ESTRUTURAS DE DECICSÃO PARA AVALIAR SE NUMBER1 É > QUE NUMBER2, VIVE-VERSA. *****\n"
-        );
+        Console.WriteLine($"***** ESTRUTURAS DE DECICSÃO PARA AVALIAR SE NUMBER1 É > QUE NUMBER2, VIVE-VERSA. *****\n");
 
         int number1 = 1000;
         int number2 = 999;
@@ -27,21 +25,17 @@ class testandoCondicionais
         {
             Console.WriteLine($"You have purchased {number2} dollars");
             Console.WriteLine($"You may receive {descount2} dollars of discont.");
+            Console.WriteLine();
         }
 
-        Console.WriteLine("\n");
-        Console.WriteLine(
-            $"***** ABAIXO SEGUE O MESMO EXERCÍCIO, MAS UTILIZANDO OPERADORES CONDICIONAIS - TERNÁRIOS. *****"
-        );
-        Console.WriteLine("\n");
+        Console.WriteLine($"***** ABAIXO SEGUE O MESMO EXERCÍCIO, MAS UTILIZANDO OPERADORES CONDICIONAIS - TERNÁRIOS. *****\n");
         //  >1000 = 100 ? <1000 : 50;
 
-        // int saleAmount = 1001;
-        // int discount = saleAmount > 1000 ? 100 : 50;
-        // Console.WriteLine($"Discount: {discount}");
-
         int saleAmount = 1001;
-
-        Console.WriteLine($"Discount: {(saleAmount > 100 ? 100 : 50)}");
+        int discount = saleAmount > 1000 ? 100 : 50;
+        Console.WriteLine($"Discount: {discount}");
+        // OR...
+        int saleAmount2 = 1001;
+        Console.WriteLine($"Discount: {(saleAmount2 > 100 ? 100 : 50)}");
     }
 }
