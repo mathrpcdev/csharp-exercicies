44c5a6910eca4045344bbda49829afadfa3355d8
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-01 12:25:28 -0300

    Melhorias no código e na leitura. 1/11/22

--- fraudulentOrders.cs
+++ fraudulentOrders.cs
@@ -4,48 +4,56 @@ class fraudulentOrderIDs
 {
     static void Main(string[] args)
     {
-        /* CRIANDO UM SÍMBOLO ARRAY QUE ARMAZENARÁ UMA QUANTIDADE DE VETORES(?), INICIADAS
-         UMA UMA INSTÂNCIA NA MEMÓRIA CONTENDO A QUANTIDADE DE STORAGES
-         O SEGUNDO ARRAY, PARA EXEISTIR, SEMPRE INICIARÁ COM ALGO.
-         EX: [] VAR [3]     */
+        /* 
+           Neste exercício estou criando/instanciando um array de uma dimenssão contendo 3 objetos.
+           Documentação utilizada: https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/
+           "Arrays are zero indexed: an array with n elements is indexed from 0 to n-1".
+        
+           O SEGUNDO ARRAY, PARA EXEISTIR, SEMPRE INICIARÁ COM ALGO.
+           EX: int[] exem1 = new int[3];    
+        */
 
-        // ARRAYS ARE ZERO BASE .
+        // ARRAYS SÃO DE BASE ZERO.
         string[] fraudulentOrderIDs = new string[3];
         fraudulentOrderIDs[0] = "A123";
         fraudulentOrderIDs[1] = "B456";
         fraudulentOrderIDs[2] = "C789";
-        //fraudulentOrderIDs[3] = "D000"; 
+        //fraudulentOrderIDs[3] = "D000";  <-- new string [3] significa que vai do [0] até o [2].
 
         // ----------------------------------------------------------------------------------------------------------------------
 
-        // ESTES ARRAYS ESTÃO INVOCANDO UMA STRING DE CARACTERES.
-
+        //  INVOCANDO ARRAYS DE CARACTERES.
         Console.WriteLine($"First array: {fraudulentOrderIDs[0]} ");
         Console.WriteLine($"First array: {fraudulentOrderIDs[1]} ");
         Console.WriteLine($"First array: {fraudulentOrderIDs[2]} ");
 
-        fraudulentOrderIDs[0] = "F00";
+        fraudulentOrderIDs[0] = "F00"; // ESTE ARRAY [0] É DIFERENTE DO PRIMEIRO [0] ARRAY ("A123)"
         Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
 
         // -----------------------------------------------------------------------------------------------------------------
 
+        // OUTRA MANEIRA DE INICIALIZAR UM ARRAY.
+        string[] fraudulentOrderIDs2 = {"A123", "B456", "C789"};
 
-        //  string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
-
-        Console.WriteLine($"First inicialization: {fraudulentOrderIDs[0]}");
-        Console.WriteLine($"Second inicialization:{fraudulentOrderIDs[1]}");
-        Console.WriteLine($"Third inicialization: {fraudulentOrderIDs[2]}");
+        Console.WriteLine($"First inicialization: {fraudulentOrderIDs2[0]}");
+        Console.WriteLine($"Second inicialization:{fraudulentOrderIDs2[1]}");
+        Console.WriteLine($"Third inicialization: {fraudulentOrderIDs2[2]}");
 
         fraudulentOrderIDs[0] = "F000";
 
         //INVOCANDO O CONTEÚDO DO PRIMEIRO "STORAGE" DO ARRAY 0. NO CASO O CONTEÚDO É UMA STRING DE CARACTERES.
         Console.WriteLine($"Reassing first inicialization: {fraudulentOrderIDs[0]}");
 
-        //.Length MOSTRARÁ A QUANTIDADE TOTAL DE MATRIZES(OU VETORES?)
-        Console.WriteLine($"Thereare {fraudulentOrderIDs.Length} fraudulent orders to process.");
+        //.Length MOSTRARÁ A QUANTIDADE TOTAL DE OBJETOS DENTRO DO ARRAY).
+        Console.WriteLine($"Thereare {fraudulentOrderIDs2.Length} fraudulent orders to process.");
 
         // -------------------------- ULTIMO EXERCÍCIO ANTES DO CHALLENGE -----------------------
-
+        /* 
+           A cada objeto do array, será referênciada à uma nova variável chamada "name",
+           e depois irá imprimir essa nova variável de a cordo com a quatidade dos objetos contido nos arrays.
+           Ex: string names = "Matheus", "Alexandrina", "Roney";
+           string armazenaObjetos = names;
+        */
         string[] names = { "Bob", "Conrad", "Grant" };
         foreach (string name in names)
         {
