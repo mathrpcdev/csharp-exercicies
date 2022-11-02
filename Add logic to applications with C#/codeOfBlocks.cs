a271ec4224cae3f459dd21e341bf6f631b94b5bd
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-02 17:02:08 -0300

    first commit

---
+++ Add logic to applications with C#/codeOfBlocks.cs
@@ -0,0 +1,14 @@
+﻿/* 
+Este exercício serve para demonstrar que as variáveis dentro de um bloco de código IF é apenas visível dentro de 
+seu próprio bloco de código. Variáveis locais != Variáveis globais.
+ */
+
+bool flag = true;
+int value = 0;
+
+if(flag)
+{
+    value = 10;
+    Console.WriteLine("Inside of code block: " + value);
+}
+Console.WriteLine($"Outside of code block: {value}");
