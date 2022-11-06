741889e1d4528544257a0b949345c5cf357417d0
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-06 15:03:18 -0300

    First commit

---
+++ Add logic to applications with C#/blocosDeCodigoSemChaves.cs
@@ -0,0 +1,14 @@
+﻿/*
+Neste exercício aprendemos a como não usar as chaves de blocos de código
+caso isso deixe o código mais limpo e legível.
+Fazemos isso caso haja apenas uma linha em um bloco de código.
+ */
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
