e597cef68692081014702f65bf8e6ee8b7e7690c
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-11-06 14:10:18 -0300

    First commit
    É demonstrado os limites de visibilidade no código entre namespaces, classes e métodos.

---
+++ Add logic to applications with C#/MyApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MyApp.Utilities; // Eu também posso classes utilizando usings. Tornando o código mais simples.
+
+/*
+Neste exemplo entendemos que
+ */
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = Utility.Reverse(value);
+            Console.WriteLine($"Reversed Value message: {reversedValue}");
+        }
+    }
+}
+
+namespace MyApp.Utilities // Foi movida a classe Utility para dentro de um novo namespace (MyApp.Utilities)
+{
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
