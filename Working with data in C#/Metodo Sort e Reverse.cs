23780d811c39338ba810b77b73b14f3725c96444
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2023-01-05 12:34:08 -0300

    First commit

---
+++ Working with data in C#/Metodo Sort e Reverse.cs
@@ -0,0 +1,20 @@
+﻿/* 05/01/23
+Métodos de ordenações de Arrays Sort() e Reverse().
+Ambos os métodos seguem a ordem crescente e alfanumérica.
+ */
+
+string[] algo = { "B14", "A11", "B12", "A13" };
+Array.Sort(algo); // Ordena em forma crescente e alfanumérica.
+Console.WriteLine("Ordene...");
+foreach (var algos in algo)
+{
+    Console.WriteLine($" -- {algos}");
+}
+
+Console.WriteLine("");
+
+Array.Reverse(algo); // Ordena em forma decrescente e alfanumérica.
+foreach (var algos in algo)
+{
+    Console.WriteLine($" -- {algos}");
+}
